6855ad715aab2ead7e56405915a759fb44f0f1bf
craneo2099 <crankras@hotmail.com>
2021-08-22 00:05:14 -0500

    nuevo post El futuro de Java

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ gulp.task('serve', gulp.series(gulp.parallel('sass'), () => {
 	], gulp.parallel('sass'));
 
 	gulp.watch('src/*.html').on('change', browserSync.reload);
+	gulp.watch('src/*.php').on('change', browserSync.reload);
 	gulp.watch('src/blog/*/*.html').on('change', browserSync.reload);
 }));
 
